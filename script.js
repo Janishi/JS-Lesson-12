e6e5d431cfc5338e764244e2749894a7a84892c5
Janishi <106867667+Janishi@users.noreply.github.com>
2022-09-01 18:18:00 +0400

    added email validation

--- script.js
+++ script.js
@@ -72,3 +72,31 @@ icon.addEventListener('click', function(){
 
     }
 })
+
+function validationEmail (){
+    let form = document.getElementById('form');
+    let email = document.getElementById ('email2').value;
+    let errortext = document.getElementById('text');
+
+    let emailStructure = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (email.match(emailStructure)){
+        form.classList.add('valid');
+        errortext.innerHTML = 'Your Email is valid';
+        errortext.style.color = 'green';
+    
+        
+    } else {
+        form.classList.remove('valid');
+        form.classList.add('invalid');
+        errortext.innerHTML = 'Your Email is invalid'
+        errortext.style.color = 'red';
+
+    }
+
+    if (email == ""){
+        form.classList.remove('valid');
+        form.classList.remove('invalid');
+        errortext.innerHTML = "enter your email";
+    }
+}
